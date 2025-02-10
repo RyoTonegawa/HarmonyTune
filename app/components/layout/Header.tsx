651c9778643eadd9952264a9e82fe44fa1af2990
RyoTonegawa <tonegawa622@gmail.com>
2025-02-11 01:14:26 +0900

    DarkMode追加

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,7 +1,11 @@
+import { ModeToggle } from "../modeToggle/mode-toggle";
+
 const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 dark:bg-background">
       <h1 className="text-2xl font-bold text-muted-foreground">Harmony Tune</h1>
+
+      <ModeToggle />
     </header>
   );
 };
